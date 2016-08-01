0b848c8faa7f1bbb5cbfff8ee755d95994bdb6b4
Remon Lam <remon.lam@lamnet.nl>
2016-08-01 18:27:26 +0200

    fix download location

--- functions/functionGetFedoraSource.sh
+++ functions/functionGetFedoraSource.sh
@@ -3,7 +3,7 @@
 function functionGetFedoraSource () {
   echo "Downloading Fedora image ${_variant}"
   mkdir -p /root/temp/
-  wget -c download.fedoraproject.org/pub/fedora/linux/releases/24/Spins/armhfp/images/Fedora-${_variant}-armhfp-24-1.2-sda.raw.xz
+  wget -c -P /root/temp/ download.fedoraproject.org/pub/fedora/linux/releases/24/Spins/armhfp/images/Fedora-${_variant}-armhfp-24-1.2-sda.raw.xz
   xz -kd $firm_location/Fedora-${_variant}-armhfp-24-1.2-sda.raw.xz
   _offset=$(partx /root/temp/Fedora-${_variant}-armhfp-24-1.2-sda.raw | tail -n 1 | awk '{print $2}')
   _offset=$((_offset*512))
